d5c6d21bdd9061c0cc896098b4f35469cb078057
gabrielruggie <gruggie77@gmail.com>
2023-10-10 01:01:18 +0000

    continued development

--- phase_1/FlagRegister.v
+++ phase_1/FlagRegister.v
@@ -1,15 +1,15 @@
 module FlagRegister (
 
-    input clk, rst, enable,
-    input [2:0] flag_prev,
+    input clk, rst,
+    input [2:0] flag_prev, enable,
     output [2:0] flag_curr,
 
 );
 
 
-    dff Z ( .q(flag_curr[0]), .d(flag_prev[0]), .wen(enable), .clk(clk), .rst(!rst_n));
-    dff V ( .q(flag_curr[1]), .d(flag_prev[1]), .wen(enable), .clk(clk), .rst(!rst_n));
-    dff N ( .q(flag_curr[2]), .d(flag_prev[2]), .wen(enable), .clk(clk), .rst(!rst_n));
+    dff Z ( .q(flag_curr[0]), .d(flag_prev[0]), .wen(enable[0]), .clk(clk), .rst(!rst_n));
+    dff V ( .q(flag_curr[1]), .d(flag_prev[1]), .wen(enable[1]), .clk(clk), .rst(!rst_n));
+    dff N ( .q(flag_curr[2]), .d(flag_prev[2]), .wen(enable[2]), .clk(clk), .rst(!rst_n));
 
 
 
--- phase_1/cpu.v
+++ phase_1/cpu.v
@@ -8,24 +8,27 @@ module CPU (
 );
 
     // Module Inputs //
+    wire im_wr, dm_wr, dm_en;
     wire [15:0] alu_in, data_memory_in, imemory_in;
+    wire [2:0] flag_prev, flag_curr, flag_en;
 
     // Module Outputs //
     wire [15:0] alu_out, data_memory_out, imemory_out;
 
-    // PC Control Unit //
+    // PC Unit //
 
-    // CPU Control Unit //
+    // Control Unit //
 
     // Instruction Memory //
-    InstructionMemory im ( .data_in(imemory_in), .data_out(imemory_out), .addr(), .enable(), .wr(), .clk(clk), .rst(!rst_n) );
+    InstructionMemory im ( .data_in(imemory_in), .data_out(imemory_out), .addr(pc), .enable(!halt), .wr(im_wr), .clk(clk), .rst(!rst_n) );
 
-    // Flag Register //
+    // Flag Register /
+    FlagRegister fr ( .clk(clk), .rst(!rst_n), .enable(flag_en), .flag_prev(flag_prev), .flag_curr(flag_curr) );
 
     // Register File //
     RegisterFile rf ( .clk(clk), .rst(!rst_n), .src_reg1(), .src_data2(), .dst_reg(), .write_en(), .dst_data(), .src_data1(), .src_data2() );
 
     // Data Memory
-    DataMemory dm ( .data_in(data_memory_in), .data_out(data_memory_out), .addr(), .enable(), .wr(), .clk(clk), .rst(!rst_n) );
+    DataMemory dm ( .data_in(data_memory_in), .data_out(data_memory_out), .addr(alu_out), .enable(dm_en), .wr(dm_wr), .clk(clk), .rst(!rst_n) );
 
 endmodule
