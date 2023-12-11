070158170b9ee673f6e0b6c2a34630bea9dffe0a
gabrielruggie <gruggie77@gmail.com>
2023-12-11 17:01:39 +0000

    continued development

--- phase_3/CacheController.v
+++ phase_3/CacheController.v
@@ -5,17 +5,28 @@ module CacheController (
     input [15:0] iaddress, daddress, data_in,
 
     output fetch_stall, mem_stall,
-    output ihit, dhit,
+    output icache_hit, dcache_hit,
     output [15:0] instruction_out, data_out
 
 );
 
     wire [63:0] iblock_sel, dblock_sel;
-    wire [7:0] ioffset_sel, doffset_sel;
+    wire [7:0] iword_sel, dword_sel, word_sel;
     wire [7:0] itag0_nxt, itag1_nxt, dtag0_nxt, dtag1_nxt;
     wire [7:0] itag0_curr, itag1_curr, dtag0_curr, dtag1_curr;
 
     wire write_tag_array;
+    wire stall;
+
+    wire icache_miss, dcache_miss;
+    wire iwrite0, iwrite1, dwrite0, dwrite1;
+    wire iwrite_w0, iwrite_w1, dwrite_w0, dwrite_w1;
+
+    wire [15:0] mem_data;
+    wire [15:0] miss_addr;
+    wire [15:0] icache_data_in, dcache_data_in;
+
+    wire [7:0] icache_word_sel, dcache_word_sel;
 
     // iCache
     dff instr_tag_arr0[7:0] ( .clk(clk), .rst(rst), .wen(1'b1), .d(itag0_nxt), .q(itag0_curr));
@@ -30,14 +41,14 @@ module CacheController (
     Decoder6to64 dblock ( .data_in(daddress[9:4]), .wordline(dblock_sel) );
 
     // Decoders to select correct word
-    Decoder3to8 ioffset ( .data_in(iaddress[3:1]), .wordline(ioffset_sel) );
-    Decoder3to8 doffset ( .data_in(daddress[3:1]), .wordline(doffset_sel) );
+    Decoder3to8 iword ( .data_in(iaddress[3:1]), .wordline(iword_sel) );
+    Decoder3to8 dword ( .data_in(daddress[3:1]), .wordline(dword_sel) );
 
     // Cache Miss State Machine
-    CacheFillFsm cache_fsm ( .clk(clk), .rst(rst), .miss_detected(), .mem_data_vld(), .miss_addr(), .w0(), .w1(), .fsm_busy(), .write_data_array(), 
-                             .write_tag_array(), .mem_address(), .sel() );
+    CacheFillFsm cache_fsm ( .clk(clk), .rst(rst), .miss_detected(), .mem_data_vld(), .miss_addr(), .w0(), .w1(), .fsm_busy(stall), .write_data_array(), 
+                             .write_tag_array(), .mem_address(), .sel(word_sel) );
 
-    MultiCycleMemory multi_mem ( .data_out(), .data_in(), .addr(), .enable(), .wr(), .clk(), .rst(), .data_valid() );
+    MultiCycleMemory multi_mem ( .data_out(mem_data), .data_in(), .addr(), .enable(), .wr(), .clk(), .rst(), .data_valid() );
 
     assign cache_enable = iwrite | iread | dwrite | dread;
 
@@ -48,8 +59,30 @@ module CacheController (
     assign iwrite_w1 = ( (itag0_curr[6] == 0) && !iwrite_w0) ? 1'b1 : itag1_curr[7];
 
     assign dwrite0 = ( (daddress[15:10] == dtag0_nxt[5:0]) && dtag0_nxt[6] ) && !write_tag_array;
-    assign dwrite0 = ( (daddress[15:10] == dtag1_nxt[5:0]) && dtag1_nxt[6] ) && !write_tag_array;
+    assign dwrite1 = ( (daddress[15:10] == dtag1_nxt[5:0]) && dtag1_nxt[6] ) && !write_tag_array;
     assign dwrite_w0 = ( dtag0_curr[6] == 0 ) ? 1'b1 : dtag0_curr[7];
     assign dwrite_w1 = ( (dtag1_curr[6] == 0) && !dwrite_w0) ? 1'b1 : dtag1_curr[7];
+
+    assign icache_miss = ( !iwrite0 && !iwrite1 ) && (iwrite || iread);
+    assign dcache_miss = ( !dwrite0 && !dwrite1 ) && (dwrite || dread);
+
+
+    assign miss_addr = icache_miss ? iaddress : daddress;
+
+    assign dcache_data_in = dcache_miss ? mem_data : data_in;
+    assign icache_data_in = mem_data;
+
+    assign icache_word_sel = !icache_miss ? iword_sel : word_sel;
+    // May be iword_sel
+    assign dcache_word_sel = !dcache_miss ? dword_sel : word_sel;
+
+    // Output Signals //
+
+    assign fetch_stall = ( iread || iwrite ) && stall;
+    assign mem_stall = ( dread || dwrite ) && stall;
+    assign icache_hit = !icache_miss && ( iwrite || iread );
+    assign dcache_hit = !dcache_miss && ( dwrite || dread );
+
+    // Output Signals //
 
 endmodule
