348d270b4d94353443bd13cd42f66a338a7bcf52
hcarranza01 <121695645+hcarranza01@users.noreply.github.com>
2023-12-18 13:43:36 -0600

    added tag output to caches

--- phase_3/I_Cache.v
+++ phase_3/I_Cache.v
@@ -15,15 +15,17 @@ module I_Cache (
 	input [7:0]  word,		// word to select
 	input [63:0] block_en,	// selects which block to read/write
    output [15:0] data_out_0,// data recieved from cache
-   output [15:0] data_out_1
+   output [15:0] data_out_1,
+   output [7:0]  tag_out_0,
+   output [7:0]  tag_out_1
 
 );
 	
 	// instantiate 2 data/metadata arrays for way 0 and way 1 to read from cache in one cycle
 	DataArray dataArray_0(.clk, .rst, .DataIn(data_in), .Write(write_en), .BlockEnable(block_en), .WordEnable(word), .DataOut(data_out_0));
 	DataArray dataArray_1(.clk, .rst, .DataIn(data_in), .Write(write_en), .BlockEnable(block_en), .WordEnable(word), .DataOut(data_out_1));
-	MetaDataArray metaDataArray_0(.clk, .rst, .DataIn(tag_in_0), .Write(write_en), .BlockEnable(block_en), .DataOut());
-	MetaDataArray metaDataArray_1(.clk, .rst, .DataIn(tag_in_1), .Write(write_en), .BlockEnable(block_en), .DataOut());
+	MetaDataArray metaDataArray_0(.clk, .rst, .DataIn(tag_in_0), .Write(write_en), .BlockEnable(block_en), .DataOut(tag_out_0));
+	MetaDataArray metaDataArray_1(.clk, .rst, .DataIn(tag_in_1), .Write(write_en), .BlockEnable(block_en), .DataOut(tag_out_1));
 
 
 endmodule
